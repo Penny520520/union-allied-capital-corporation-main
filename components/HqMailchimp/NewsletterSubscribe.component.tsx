0b5566812a4d8722c7452ae0116b33852cfddead
Sean <xzhu32@my.bcit.ca>
2022-09-12 19:59:13 -0700

    fixbug

--- components/HqMailchimp/NewsletterSubscribe.component.tsx
+++ components/HqMailchimp/NewsletterSubscribe.component.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { MailchimpSubscribe, Props } from 'react-mailchimp-subscribe';
+import MailchimpSubscribe from 'react-mailchimp-subscribe';
 import CustomForm from './CustomForm.component';
 
-declare module 'react-mailchimp-subscribe' {
-  type Props = {
-    url: any;
-    subscribe: any;
-    status: any;
-    message: any;
-  };
-  var MailchimpSubscribe: React.ComponentClass<Props>;
-}
-const NewsletterSubscribe = () => {
+// declare module 'react-mailchimp-subscribe' {
+//   type Props = {
+//     url: any;
+//     subscribe: any;
+//     status: any;
+//     message: any;
+//   };
+//   var MailchimpSubscribe: React.ComponentClass<Props>;
+// }
+// const NewsletterSubscribe = () => {
+  export default () => {
   // export default () => {
   const MAILCHIMP_URL = process.env.NEXT_PUBLIC_MAILCHIMP_URL;
   // if (!MailchimpSubscribe) return null;
@@ -19,30 +20,30 @@ const NewsletterSubscribe = () => {
   return (
     <MailchimpSubscribe
       url={MAILCHIMP_URL}
-      render={({ subscribe, status, message }) => {
-        // const { subscribe, status, message } = props || {};
-        return (
-          <CustomForm
-            status={status}
-            message={message}
-            onValidated={formData => subscribe(formData)}
+      // render={(props: {}) => {
+      //   const { subscribe, status, message } = props || {};
+      //   return (
+      //     <CustomForm
+      //       status={status}
+      //       message={message}
+      //       onValidated={formData => subscribe(formData)}
 
-            //   const url = {MAILCHIMP_URL}
-            // render={(props) => {
-            //   const { subscribe, status, message } = props || {};
-            //   return (
-            //       <CustomForm
-            //         status={status}
-            //         message={message}
-            //         onValidated={formData => subscribe(formData)}
-          />
-        );
-      }}
+      //       //   const url = {MAILCHIMP_URL}
+      //       // render={(props) => {
+      //       //   const { subscribe, status, message } = props || {};
+      //       //   return (
+      //       //       <CustomForm
+      //       //         status={status}
+      //       //         message={message}
+      //       //         onValidated={formData => subscribe(formData)}
+      //     />
+      //   );
+      // }}
     />
   );
 };
 
-export default NewsletterSubscribe;
+// export default NewsletterSubscribe;
 
 // import MailchimpSubscribe from 'react-mailchimp-subscribe';
 // import NewsletterForm from './NewsletterForm';
